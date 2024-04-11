5fba87b328eca0788e232899495314646205e6cb
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2024-04-11 10:56:31 -0400

    April 11 morning work, some reorganization

--- Apr_8_scripts/Apr8_batchEVOKED.m
+++ Apr_8_scripts/Apr8_batchEVOKED.m
@@ -1,3 +1,5 @@
+function apr8_batchEvoked(datadir,outputfile, last_only)
+
 %This script finds the firing properties of the FIRST AP ever detected in a
 %sweep with at least 2 APs
 %from each cell in the directory, and exports the summary table as an excel
@@ -7,30 +9,30 @@
 % minegishis@brandeis.edu
 % Apr 8 2024
 
+if nargin<3,
+    last_only = 0;
+end;
+
  %start loading files
 close all
-clf 
-clear 
+
+analysis_dir = [userpath filesep 'tools' filesep 'AP_AHP_Analysis_Iclamp_SayaMinegishi' filesep 'analyses'];
+
+dirname = [userpath filesep 'tools' filesep 'AP_AHP_Analysis_Iclamp_SayaMinegishi' filesep 'data' filesep datadir];
+
+disp(['Now working on directory ' dirname])
+
 %start loading files
 filesNotWorking = []; %list of files with errors
-list = dir('*.abf');%This script finds the firing properties of the FIRST AP ever detected
+list = dir([dirname filesep '*.abf']);%This script finds the firing properties of the FIRST AP ever detected
 %from each cell in the directory, and exports the summary table as an excel
 %file.
-
-% Created by: Sayaka (Saya) Minegishi
-% minegishis@brandeis.edu
-% Apr 3 2024
-
- %start loading files
-close all
-clf 
-clear 
-%start loading files
-filesNotWorking = []; %list of files with errors
-list = dir('*.abf');
 file_names = {list.name}; %list of all abf file names in the directory 
+for i=1:numel(file_names),
+    file_names{i} = [dirname filesep file_names{i}];
+end;
 
-filenameExcelDoc = strcat(['apr8_WKYnewnew.xlsx']);
+filenameExcelDoc = strcat([analysis_dir filesep outputfile]);
 myVarnames1= {'cell_name', 'current_injected(pA)','frequency(Hz)','spike_location(ms)', 'threshold(mV)', 'amplitude(mV)', 'AHP_amplitude(mV)', 'trough value (mV)', 'trough location(ms)', 'peak value(mV)', 'peak location(ms)', 'half_width(ms)', 'AHP_30_val(mV)', 'AHP_50_val(mV)', 'AHP_70_val(mV)', 'AHP_90_val(mV)', 'half_width_AHP(ms)', 'AHP_width_10to10%(ms)', 'AHP_width_30to30%(ms)', 'AHP_width_70to70%(ms)', 'AHP_width_90to90%(ms)','AHP_width_90to30%(ms)', 'AHP_width_10to90%(ms)','risetime(ms)', 'decaytime(ms)' };
 
 multipleVariablesTable= zeros(0,numel(myVarnames1));
@@ -42,20 +44,15 @@ current_injected = [-50:15:310];
 
 for n=1:size(file_names,2)
     
-   try   
-   filename = string(file_names{n});
-    disp([int2str(n) '. Working on: ' filename{:}])
+  filename = string(file_names{n});
+  disp([int2str(n) '. Working on: ' filename{:}])
    
-         
-         M1= analyzeSingleEvokedApr8(filename, current_injected);
-         T1 = [T1; M1];
-            
-              
-             
-   catch
-
-     fprintf('Invalid data in iteration %s, skipped.\n', filename);
+   M1= analyzeSingleEvokedApr8(filename, current_injected,last_only);
+   if isempty(M1),
+    fprintf('Invalid data in iteration %s, skipped.\n', filename);
     filesNotWorking = [filesNotWorking;filename];
+   else,
+       T1 = [T1; M1];
    end
    
 
