9878818eeae9d6b05b9b99dc71a8a225af3ae13e
Sayaka (Saya) Minegishi <47896245+sayakaminegishi@users.noreply.github.com>
2024-04-13 10:06:21 -0400

    Delete Apr_8_scripts/Apr_8_scripts directory

--- Apr_8_scripts/Apr_8_scripts/april8work.m
+++
@@ -1,8 +0,0 @@
- 
-
-
-
-Apr8_batchEVOKED('wky','apr8_WKYnewnew.xlsx')
-Apr8_batchEVOKED('jason_2005','apr8_jason2005.xlsx')
-Apr8_batchEVOKED('wky','apr8_WKYnewnew_lastsweep.xlsx',1)
-Apr8_batchEVOKED('jason_2005','apr8_jason2005_lastsweep.xlsx',1)
