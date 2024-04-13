8682512d2eb687709616787363e8dddd77fe9bab
Sayaka (Saya) Minegishi <47896245+sayakaminegishi@users.noreply.github.com>
2024-04-13 10:07:15 -0400

    apr 13 updates

--- Apr_8_scripts/apr11.m
+++ Apr_8_scripts/apr11.m
@@ -1,4 +1,11 @@
+
 newpath = "/Users/sayakaminegishi/MATLAB";
 userpath(newpath)
 
-Apr8_batchEVOKED("wky","myexcel.xlsx")
+Apr8_batchEVOKED("wky","WKYevoked_apr12.xlsx")
+Apr8_batchEVOKED("shr","SHRevoked_apr12.xlsx")
+
+
+Apr8_batchEVOKED("jason_2005","jason_2005_recording5.xlsx")
+
+Apr8_batchEVOKED("jason_2005_02_04 c1","jason_2006_recordingall.xlsx")
