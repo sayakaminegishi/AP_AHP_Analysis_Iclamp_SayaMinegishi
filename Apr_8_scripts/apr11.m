44621199c541bbd6e8681fd84809d2b107593a6b
“Sayaka <saya1113saya@gmail.com>
2024-04-23 13:56:22 -0400

    apr23

--- Apr_8_scripts/apr11.m
+++ Apr_8_scripts/apr11.m
@@ -2,25 +2,11 @@
 newpath = "/Users/sayakaminegishi/MATLAB";
 userpath(newpath)
 
-<<<<<<< HEAD
-Apr8_batchEVOKED("wky","WKYevoked_apr15ls.xlsx")
+
+%Apr8_batchEVOKED("wky","WKYevoked_apr15ls.xlsx")
 %Apr8_batchEVOKED("shr","SHRevoked_apr15ls.xlsx",1) %1 = analyze LAST SWEEP ONLY!!!
 
 
-CMA_batch_analysisFeb17("spontaneous","2024_02_09_04_0003_analysis.xlsx")
+CMA_batch_analysisFeb17("spontaneous","spont_all_apr23.xlsx")
 %first sheet= burst properties, second sheet = avg of singlets properties
 
-% Apr8_batchEVOKED("jason_2005","jason_2005_recording5.xlsx")
-% 
-% Apr8_batchEVOKED("2005_01_13_c1","jason_2006_recordingall.xlsx")
-
-
-=======
-Apr8_batchEVOKED("wky","WKYevoked_apr15ls.xlsx",1)
-Apr8_batchEVOKED("shr","SHRevoked_apr15ls.xlsx",1)
-
-
-% Apr8_batchEVOKED("jason_2005","jason_2005_recording5.xlsx")
-% 
-% Apr8_batchEVOKED("2005_01_13_c1","jason_2006_recordingall.xlsx")
->>>>>>> 02514955f0697ce942dcce5f275a947161d57074
