e00bbc37f9f40c3d305c8e4b21c76371e4ccb810
“Sayaka <saya1113saya@gmail.com>
2024-04-24 13:05:46 -0400

    update apr24

--- Apr_8_scripts/apr11.m
+++ Apr_8_scripts/apr11.m
@@ -7,7 +7,7 @@ userpath(newpath)
 %Apr8_batchEVOKED("shr","SHRevoked_apr15ls.xlsx",1) %1 = analyze LAST SWEEP ONLY!!!
 
 
-CMA_batch_analysisFeb17("spontaneous","spont_all_apr23_2.xlsx")
+CMA_batch_analysisFeb17("spontaneous","spont_all_apr23_modifiedbinwidth.xlsx")
 
 %CMA_batch_analysisApr24("spontaneous","spont_all_apr24_testing.xlsx");
 %first sheet= burst properties, second sheet = avg of singlets properties
